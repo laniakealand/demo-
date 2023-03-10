4c41cb1e4783b076d3634fdf4e31e230c80d76fa
riaa22 <kasturiawira22@gmail.com>
2023-03-10 19:06:37 +0700

    nyoba tugas 1

---
+++ nyoba.py
@@ -0,0 +1,11 @@
+nama_panjang = input ("siapa nama kamu? =")
+NIM = input ("NIM kamu berapa? =")
+alamat = input ("di mana kamu tinggal? =")
+
+print ("nama lengkap =" , nama_panjang)
+print ("NIM =" , NIM)
+print ("alamat =" , alamat)
+
+print ("nama lengkap = kasturia wira")
+print ("NIM = IO322062")
+print ("alamat = jakarta selatan") 
